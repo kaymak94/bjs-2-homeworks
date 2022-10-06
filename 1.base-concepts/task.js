b5fe003c154945b46e8c591743435ecb7f5a76cf
Каймакчи Александр <kaymakchiao@mail.ru>
2022-10-06 18:21:22 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  const arr = [];
+  const d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    let ans1 = (-b + Math.sqrt(d)) / (2 * a);
+    let ans2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(ans1, ans2);
+  }
+  else if (d === 0) {
+    let ans1 = -b / (2 * a);
+    arr.push(ans1);
+  }
+  else if (d < 0) {
+
+  }
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+//function calculateTotalMortgage(percent, contribution, amount, date) {
+ // let totalAmount;
 
   // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+ // return totalAmount;
+
