13bfbc5977f05a8ef2025170ad1dc0b432357a4e
Каймакчи Александр <kaymakchiao@mail.ru>
2022-10-14 12:19:58 +0300

    Initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
+"use strict"
+//задача 1
 function compareArrays(arr1, arr2) {
-  let result;
+  let answer;
 
-  // Ваш код
+  answer = (arr1.length === arr2.length) && arr1.every((elment, index) => elment === arr2[index])
+
+
 
   return result; // boolean
 }
 
+//задача 2
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((number) => number > 0).filter((number) => number % 3 === 0).map((number) => number * 10)
 
   return resultArr; // array
 }
