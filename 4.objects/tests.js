d1490ce2bc692a19903fb108a8379561d9a80c29
Каймакчи Александр <kaymakchiao@mail.ru>
2022-10-18 20:55:11 +0300

    initial commit

--- 4.objects/tests.js
+++ 4.objects/tests.js
@@ -1,37 +1,37 @@
-describe("Домашнее задание к занятию 4 «Объекты»", () => {
-  describe("Задача №1 Инкапсуляция студента", () => {
-    let student1 = new Student("Tony", "male", 37);
-    student1.setSubject("Algebra");
-    student1.addMark(5);
-    student1.addMark(4); 
-    student1.addMark(5);
-    
-    let student2 = new Student("Buzz", "female", 35);
-    student2.setSubject("Geometry");
-    student2.addMarks(2,3,2);
+"use strict"
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
 
-    it("Установка предмета работает корректно", () => {
-      expect(student1.subject).toEqual("Algebra");
-      expect(student2.subject).toEqual("Geometry");
-    });
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
 
-    it("Добавление оценок addMark работает корректно", () => {
-      expect(student1.marks).toEqual([5, 4, 5]);
-    });
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
 
-    it("Добавление группы оценок addMarks работает корректно", () => {
-      expect(student2.marks).toEqual([2, 3, 2]);
-    });
+Student.prototype.addMarks = function (...array) {
+  if (this.marks === undefined) {
+    this.marks = [...array];
+  } else {
+    this.marks.push(...array);
+  }
+}
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => sum = sum + item);
+  return sum / this.marks.length;
+}
 
-    it("Вычисление среднего работает корректно", () => {
-      expect(student1.getAverage()).toEqual(14/3);
-      expect(student2.getAverage()).toEqual(7/3);
-    });
-
-    it("Отчисление студента работает корректно", () => {
-      student2.exclude('low grades')
-      expect(student2.excluded).toEqual('low grades');
-    });
-
-  });
-});
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
